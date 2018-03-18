fc5aee55fb29ec9e21fa514f0b6a2f248a103c41
Nikita Kot <kotn01@outlook.cz>
2018-03-18 23:49:45 +0100

    my rating component

--- src/components/BarList.js
+++ src/components/BarList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 import { Button, Card, CardSection } from './common';
 import NavigationService from '../utlis/NavigationService';
+import MyRating from './MyRating';
 
 class BarList extends React.Component {
     static navigationOptions = {
         title: 'BarList',
     };
+
+    ratingCompleted(rating) {
+        console.log(`Rating is: ${rating}`);
+    }
 
     render() {
         return (
@@ -20,6 +25,13 @@ class BarList extends React.Component {
                 </CardSection>
                 <CardSection>
                     <Text>Beer List</Text>
+                </CardSection>
+                <CardSection>
+                    <MyRating
+                        label={'Rating'}
+                        startingValue={4}
+                        onFinishRating={this.ratingCompleted}
+                    />
                 </CardSection>
             </Card>
         );
---
+++ src/components/MyRating.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Rating } from 'react-native-elements';
+import { TEXT_STYLE } from '../utlis/constants';
+
+export default class MyRating extends Component {
+
+    render() {
+        const { label, startingValue, onFinishRating } = this.props;
+        const { ratingStyle, containerStyle, labelStyle } = styles;
+
+        return (
+            <View style={containerStyle}>
+                <Text style={labelStyle}>{label}</Text>
+                <Rating
+                    style={ratingStyle}
+                    startingValue={startingValue}
+                    fractions={1}
+                    imageSize={35}
+                    onFinishRating={onFinishRating}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = {
+    ratingStyle: {
+        flex: 3
+    },
+    labelStyle: TEXT_STYLE,
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+};
