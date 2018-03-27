8174d95805164379c69189cff155c1f47deb60f0
Nikita Kot <kotn01@outlook.cz>
2018-03-27 19:10:15 +0200

    express configured

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,7 +1,19 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors')({ origin: true });
+const cookieParser = require('cookie-parser')();
+const app = express();
+
 admin.initializeApp(functions.config().firebase);
 
+app.use(cors);
+app.use(cookieParser);
+app.get('/add-bar', (req, res) => {
+    console.log(req.query);
+    res.json({ test: `testing data: ${req.query.test}` });
+});
 
+
+exports.app = functions.https.onRequest(app);
 
