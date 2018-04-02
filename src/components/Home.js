43fcf9cbde19bc90023d9981429a4334bb82bdd9
Nikita Kot <kotn01@outlook.cz>
2018-04-02 12:58:29 +0200

    clickable title

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import Map from './Map';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
 import { fetchBarsList } from '../actions/BarActions';
+import { Text } from 'react-native';
 
 class Home extends React.Component {
     static navigationOptions = {
@@ -16,9 +17,15 @@ class Home extends React.Component {
     renderMarkers() {
         const { barList } = this.props;
         return barList.map((bar, k) => (<MapView.Marker
-            key={k}
-            coordinate={{ latitude: bar.lat, longitude: bar.lng }}
-        />));
+                key={k}
+                coordinate={{ latitude: bar.lat, longitude: bar.lng }}
+                onCalloutPress={() => this.props.navigation.navigate('Bar', { bar })}
+        >
+                <MapView.Callout>
+                    <Text>{bar.name}</Text>
+                </MapView.Callout>
+            </MapView.Marker>)
+        );
     }
 
     render() {
