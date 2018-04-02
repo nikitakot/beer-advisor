e96b162ebf43d931003b2151dfce6e91b914ab32
Nikita Kot <kotn01@outlook.cz>
2018-04-02 12:27:09 +0200

    display all bars on home map

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import Map from './Map';
+import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { fetchBarsList } from '../actions/BarActions';
 
 class Home extends React.Component {
     static navigationOptions = {
         title: 'BeerAdvisor',
     };
 
+    componentWillMount() {
+        this.props.fetchBarsList();
+    }
+
+    renderMarkers() {
+        const { barList } = this.props;
+        return barList.map((bar, k) => (<MapView.Marker
+            key={k}
+            coordinate={{ latitude: bar.lat, longitude: bar.lng }}
+        />));
+    }
+
     render() {
         return (
             <Map
                 zoomUser
                 style={{ flex: 1 }}
-            />
+            >
+                {this.renderMarkers()}
+            </Map>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = ({ barList }) => barList;
+
+export default connect(mapStateToProps, { fetchBarsList })(Home);
 
