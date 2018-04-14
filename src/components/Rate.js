1ec17e70da5202317c4f37fca3385a74e2e0369a
Nikita Kot <nikita.kot@profinit.eu>
2018-04-14 14:55:27 +0200

    rate component v2

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Card, CardSection } from './common';
 import { Button } from './common/Button';
 import MyRating from './MyRating';
+import { Spinner } from './common/Spinner';
+import { Text } from 'react-native';
+import { ERROR_TEXT_STYLE } from '../utlis/constants';
 
 class Rate extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -12,8 +15,36 @@ class Rate extends Component {
         super(props);
         this.state = {
             loading: false,
-            rating: props.navigation.state.params.sub.rating
+            rating: props.navigation.state.params.sub.rating,
+            error: ''
         };
+    }
+
+    onPress(sub) {
+        const { onPress } = this.props.navigation.state.params;
+        this.setState({ loading: true });
+        onPress(sub)
+            .then(() => {
+                this.props.navigation.goBack();
+                this.setState({ loading: false });
+            })
+            .catch(() => {
+                this.setState({ loading: false, error: 'Error while adding rating' });
+            });
+    }
+
+    renderButton() {
+        const { sub } = this.props.navigation.state.params;
+
+        if (this.state.loading) {
+            return <Spinner size="large"/>;
+        }
+
+        return (
+            <Button onPress={() => this.onPress(sub)}>
+                Leave a rating
+            </Button>
+        );
     }
 
     render() {
@@ -26,10 +57,11 @@ class Rate extends Component {
                         startingValue={sub.rating}
                     />
                 </CardSection>
+                <Text style={ERROR_TEXT_STYLE}>
+                    {this.props.error}
+                </Text>
                 <CardSection>
-                    <Button>
-                        Leave a rating
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
